a134355924cacf1286c657640aacba896f0846ff
Rebeca Pacheco <rbkgarner@gmail.com>
2021-09-13 13:39:33 -0300

    create GlobalStyles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Nextjs Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple starter project using TypeScript, React, Nextjs and Styled components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,5 @@
-import Head from 'next/head'
+import Main from 'components/Main'
 
-type Props = {
-  title: string
-}
-
-export default function Home({ title = 'Rebeca' }: Props) {
-  return (
-    <div>
-      <Head>
-        <title> {title} </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">{title}</a>
-        </h1>
-      </main>
-    </div>
-  )
+export default function Home() {
+  return <Main />
 }
